705593ec9dda5c5f2efa31aadb1e8e40173cff94
pavin <pavin227@gmail.com>
2026-03-10 15:48:40 +0530

    fixes: fixed docker and moved the analysis to assets

--- backend/src/telemetry/index.ts
+++ backend/src/telemetry/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import { getDashboardSummary } from "./controllers/analyticsController.js";
-import verifyUser from "../users/controller/userAuthMiddleware.js";
 
 
 const router = Router();
 
-router.get('/analysis', verifyUser ,getDashboardSummary);
+
 
 export default router;
